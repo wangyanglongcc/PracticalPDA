d6d27a0a0f787d0d79fabdc1b63a234e2dc2bdeb
wangyanglong <41174364+wangyanglongcc@users.noreply.github.com>
2019-01-07 15:50:57 +0800

    Create multiple_linear_regression.py
    多元线性回归模板

---
+++ multiple_linear_regression.py
@@ -0,0 +1,25 @@
+# -*- coding: UTF-8 -*-
+import statsmodels.api as sm
+from sklearn.linear_model import LinearRegression
+
+def load_data():
+    '''
+    '''
+    return df 
+
+def reg(X,y):
+    reg = LinearRegression()
+    reg.fit(X, y)
+    # 评估回归模型
+    res = sm.add_constant(X)
+    est = sm.OLS(y, res)
+    est = est.fit()
+    return est
+
+def main():
+    df = load_data()
+    X,y = df['y'].value,df[['x1','x2','x3']]
+    est = reg(X,y)
+    print(est)
+if __name__ == '__main__':
+    main()
