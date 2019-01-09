b3d62512499b999f9c08646da11c9471ae4f7e4d
wangyanglongcc <41174364+wangyanglongcc@users.noreply.github.com>
2019-01-09 14:42:14 +0800

    Create seaborn_heatmap配色参考.py

---
+++ seaborn_heatmap配色参考.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import matplot.pyplot as plt
+import seaborn as sns
+%matplotlib inline
+
+f, ax = plt.subplots(figsize=(16, 6))
+goods = set(items.loc[items['cor']>0,'tmitemid'])
+corr = df.loc[df['tmitemid'].isin(goods),[y]+X].corr()
+hm = sns.heatmap(round(corr,2), annot=True, ax=ax, cmap="coolwarm",fmt='.2f',vmin=-1,vmax=1,
+                  linewidths=.05)
+f.subplots_adjust(top=0.93)
