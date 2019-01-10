80a32c39749d3296f3226943a0d2db41f214b535
wangyanglongcc <41174364+wangyanglongcc@users.noreply.github.com>
2019-01-10 14:19:49 +0800

    Update seaborn中lineplot用法.py

--- seaborn中lineplot用法.py
+++ seaborn中lineplot用法.py
@@ -8,3 +8,7 @@ rs = np.random.RandomState(365)
 values = rs.randn(365, 7).cumsum(axis=0)
 dates = pd.date_range("1 1 2016", periods=365, freq="D")
 data = pd.DataFrame(values, dates, columns=["A", "B", "C", "D","E","F","G"])
+
+f, ax = plt.subplots(figsize=(16, 6))
+# sns.lineplot一次最多支持6个类别
+sns.lineplot(data=data.drop('G',1), palette="tab10", linewidth=2.5);
