e85813f51692b22eabf6a86254765f49c3c00de0
wangyanglongcc <41174364+wangyanglongcc@users.noreply.github.com>
2019-01-22 10:42:24 +0800

    Create 统计DataFrame中获取每列唯一值个数.py

---
+++ 统计DataFrame中获取每列唯一值个数.py
@@ -0,0 +1,16 @@
+import pandas as pd
+# 从github上获取数据
+url = 'https://raw.githubusercontent.com/wangyanglongcc/pandas_basic/master/data/iris.csv'
+df = pd.read_csv(url)
+# 使用列表生成式统计每列唯一值个数
+ii = [(i,df[i].unique().size) for i in df.columns]
+print(ii)
+# [('sepal_length', 35), ('sepal_width', 23), ('petal_length', 43), ('petal_width', 22), ('class', 3)]
+# 将生成的结果直接转成DataFrame
+res = pd.DataFrame([(col, df[col].nunique()) for col in df.columns],columns=['col_name','value_unique'])
+# col_name	value_unique
+# 0	sepal_length	35
+# 1	sepal_width	23
+# 2	petal_length	43
+# 3	petal_width	22
+# 4	class	3
