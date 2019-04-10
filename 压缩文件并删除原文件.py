d5de77e97f2800e88377494cf006e5a0cb45bb38
wangyanglongcc <41174364+wangyanglongcc@users.noreply.github.com>
2019-04-10 10:46:45 +0800

    Create 压缩文件并删除原文件.py

---
+++ 压缩文件并删除原文件.py
@@ -0,0 +1,24 @@
+import os
+import glob
+import zipfile
+
+# 压缩文件并删除原文件
+def to_zip(local_path, filename):
+    ori_basename = os.path.basename(filename)
+    fname, ext = os.path.splitext(ori_basename)
+    zip_basename = ori_basename.replace(ext, '.zip')
+    zipfilename = os.path.join(local_path, zip_basename)
+    # 创建压缩文件
+    azip = zipfile.ZipFile(zipfilename, 'w')
+    # 写入原文件
+    azip.writestr(ori_basename, open(filename, 'rb').read(), compress_type=zipfile.ZIP_DEFLATED)
+    azip.close()
+    # 删除原文件
+    os.remove(filename)
+    return zipfilename
+
+if __name__ == '__main__':
+    local_path = 'xxx'
+    for filename in glob.glob(os.path.join(local_path,'*.*')):
+        print(filename)
+        to_zip(local_path,filename)
