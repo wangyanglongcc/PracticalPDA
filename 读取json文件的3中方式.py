3fddaaf02eb1977e5c22eba01655848d06708d5e
wangyanglongcc <41174364+wangyanglongcc@users.noreply.github.com>
2019-04-15 16:24:20 +0800

    Update 读取json文件的3中方式.py

--- 读取json文件的3中方式.py
+++ 读取json文件的3中方式.py
@@ -1,38 +1,47 @@
-方式1：
-```python
-#  读取json文件
-def read_json(filename):
-    import json
-    with open(filename, 'r+', encoding='utf-8') as f:
-        lines = f.readlines()
-    js_lines = []
-    for line in lines:
-        try:
-            js_lines.append(json.loads(line))
-        except:
-            try:
-                temp_line = line[:-2]
-                js_lines.append(json.loads(temp_line))
-            except:
-                print('*' * 20, '\n', line[:-2])
-    df = pd.DataFrame(js_lines, index=list(range(len(lines))))
-    return df
-```
-方式2：
-```python
-def load_json(filepath,encoding='utf-8'):
-    import json
-    f = open(filepath,'r+',encoding=encoding)
-    df = json.load(f)
-    f.close()
-    return pd.DataFrame(df)
-```
-方式3：
-```python
-def load_json(inputfile,encoding='utf-8'):
+def read_json(inputfile,encoding='utf-8'):
     import pandas as pd
-    f = open(filepath,'r+',encoding=encoding)
-    df = pd.read_json(f,lines=True,encoding=encoding)
-    f.close()
+    import json
+    # method 1
+    def read_json1(inputfile):
+        f = open(inputfile,'r+',encoding=encoding)
+        df = pd.read_json(f,lines=True,encoding=encoding)
+        f.close()
+        return df
+    # method 2
+    def read_json2(inputfile):
+        f = open(inputfile,'r+',encoding=encoding)
+        df = json.load(f)
+        f.close()
+        df = pd.DataFrame(df)
+        return df
+    # method 3
+    def read_json3(inputfile):
+        with open(inputfile, 'r+', encoding=encoding) as f:
+            lines = f.readlines()
+        js_lines = []
+        for line in lines:
+            try:
+                js_lines.append(json.loads(line))
+            except:
+                try:
+                    temp_line = line[:-2]
+                    js_lines.append(json.loads(temp_line))
+                except:
+                    print('*' * 20, '\n', line[:-2])
+        df = pd.DataFrame(js_lines, index=list(range(len(lines))))
+        return df
+    # try-except
+    try:
+        df = read_json1(inputfile)
+#         print(1)
+    except:
+        try:
+            df = read_json2(inputfile)
+#             print(2)
+        except:
+            df = read_json3(inputfile)
+#             print(3)
     return df
-```
+if __name__ == '__main__':
+    df = read_json(r'D:\yuqing\19_2019_3_31_pcauto_article.json')
+    print(df.shape)
