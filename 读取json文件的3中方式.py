44d534a9a75ae5539ce43d7160a2bd76f0cecd62
wangyanglongcc <41174364+wangyanglongcc@users.noreply.github.com>
2019-04-15 16:25:07 +0800

    Update 读取json文件的3中方式.py

--- 读取json文件的3中方式.py
+++ 读取json文件的3中方式.py
@@ -1,19 +1,21 @@
-def read_json(inputfile,encoding='utf-8'):
+def read_json(inputfile, encoding='utf-8'):
     import pandas as pd
     import json
     # method 1
     def read_json1(inputfile):
-        f = open(inputfile,'r+',encoding=encoding)
-        df = pd.read_json(f,lines=True,encoding=encoding)
+        f = open(inputfile, 'r+', encoding=encoding)
+        df = pd.read_json(f, lines=True, encoding=encoding)
         f.close()
         return df
+
     # method 2
     def read_json2(inputfile):
-        f = open(inputfile,'r+',encoding=encoding)
+        f = open(inputfile, 'r+', encoding=encoding)
         df = json.load(f)
         f.close()
         df = pd.DataFrame(df)
         return df
+
     # method 3
     def read_json3(inputfile):
         with open(inputfile, 'r+', encoding=encoding) as f:
@@ -30,18 +32,21 @@ def read_json(inputfile,encoding='utf-8'):
                     print('*' * 20, '\n', line[:-2])
         df = pd.DataFrame(js_lines, index=list(range(len(lines))))
         return df
+
     # try-except
     try:
         df = read_json1(inputfile)
-#         print(1)
+    #         print(1)
     except:
         try:
             df = read_json2(inputfile)
-#             print(2)
+        #             print(2)
         except:
             df = read_json3(inputfile)
-#             print(3)
+    #             print(3)
     return df
+
+
 if __name__ == '__main__':
     df = read_json(r'D:\yuqing\19_2019_3_31_pcauto_article.json')
     print(df.shape)
