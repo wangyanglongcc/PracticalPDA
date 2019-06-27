15a12ca11e338d9db4597622b0590160c5139e4d
wangyanglongcc <41174364+wangyanglongcc@users.noreply.github.com>
2019-06-27 14:19:13 +0800

    Update 查看内存使用情况.py

--- 查看内存使用情况.py
+++ 查看内存使用情况.py
@@ -1,9 +1,23 @@
 import os
 import psutil
+from collections import OrderedDict
+
 def show_memory_info(hint=None):
+    memory_convent = 1024 * 1024 * 1024
+    mem_info = psutil.virtual_memory()
     pid = os.getpid()
     p = psutil.Process(pid)
-    info = p.memory_full_info()
-    memory = info.uss/1024/1024/1024
-    # print('{} memory used: {:.4f}GB'.format(hint,memory))
-    return memory
+    mem_pid = p.memory_full_info().uss
+    mem_used = mem_info.used
+    mem_ava = mem_info.available
+    mem_total = mem_info.total
+    mem_info = OrderedDict()
+    mem_info['mem_pid'] = '{:.4f}GB'.format(mem_pid/memory_convent)
+    mem_info['mem_used'] = '{:.4f}GB'.format(mem_used/memory_convent)
+    mem_info['mem_ava'] = '{:.4f}GB'.format(mem_ava/memory_convent)
+    mem_info['mem_total'] = '{:.4f}GB'.format(mem_total/memory_convent)
+    print('{} memory info: {}'.format(hint,mem_info))
+    return mem_info
+
+if __name__ == '__main__':
+    show_memory_info('T')
