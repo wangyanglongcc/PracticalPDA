5a2310d6658bc5303a21c468eff75084f4330300
wangyanglongcc <41174364+wangyanglongcc@users.noreply.github.com>
2019-06-27 14:20:19 +0800

    Update 查看内存使用情况.py

--- 查看内存使用情况.py
+++ 查看内存使用情况.py
@@ -7,15 +7,11 @@ def show_memory_info(hint=None):
     mem_info = psutil.virtual_memory()
     pid = os.getpid()
     p = psutil.Process(pid)
-    mem_pid = p.memory_full_info().uss
-    mem_used = mem_info.used
-    mem_ava = mem_info.available
-    mem_total = mem_info.total
     mem_info = OrderedDict()
-    mem_info['mem_pid'] = '{:.4f}GB'.format(mem_pid/memory_convent)
-    mem_info['mem_used'] = '{:.4f}GB'.format(mem_used/memory_convent)
-    mem_info['mem_ava'] = '{:.4f}GB'.format(mem_ava/memory_convent)
-    mem_info['mem_total'] = '{:.4f}GB'.format(mem_total/memory_convent)
+    mem_info['mem_pid'] = '{:.4f}GB'.format(p.memory_full_info().uss/memory_convent)
+    mem_info['mem_used'] = '{:.4f}GB'.format(mem_info.used/memory_convent)
+    mem_info['mem_ava'] = '{:.4f}GB'.format(mem_info.available/memory_convent)
+    mem_info['mem_total'] = '{:.4f}GB'.format(mem_info.total/memory_convent)
     print('{} memory info: {}'.format(hint,mem_info))
     return mem_info
 
