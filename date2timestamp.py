fe1f5b779c20cbbc2db1891df10a841b46454f99
wangyanglongcc <wangyanglongcc@users.noreply.github.com>
2019-07-03 10:55:59 +0800

    add date2timestamp
    date2timestamp

---
+++ date2timestamp.py
@@ -0,0 +1,11 @@
+import time
+def mktime(yyyymmdd):
+    '''
+    把一个yyyymmdd格式的文本或字符转成时间戳
+    '''
+    t = time.strptime(str(yyyymmdd),'%Y%m%d')
+    t = int(time.mktime(t))
+    return t
+if __name__ == '__main__':
+    print(mktime(20190702)) # 1561996800
+    print(mktime(20190703)) # 1562083200
