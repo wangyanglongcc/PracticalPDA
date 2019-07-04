9ea1a32f0172e8cd1c6f3dc03025e5b259c646e1
wangyanglongcc <wangyanglongcc@users.noreply.github.com>
2019-07-04 17:16:08 +0800

    when outputfile is None
    when outputfile is None
    then outputfile =
    jsonfile.strip('.json') + '.xlsx'

--- json2xlsx.py
+++ json2xlsx.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import math
 import json
-def json2xlsx(jsonfile,xlsxfile,encoding='utf-8',max_rows=10):
+def json2xlsx(jsonfile,xlsxfile=None,encoding='utf-8',max_rows=10):
     '''
     该程序仅适用于单层的json格式，如有多层，则不适合。
     {键1:值1,键2:值2,键3:值3......}
@@ -16,10 +16,11 @@ def json2xlsx(jsonfile,xlsxfile,encoding='utf-8',max_rows=10):
             for line in lines:
                 tmp = pd.DataFrame([json.loads(line.strip())])
                 df = df.append(tmp)
-    print(df.shape)
     df = df.drop_duplicates()
     pages = math.ceil(df.shape[0]/max_rows)
     print('共有{}行{}列数据，将会写出{}个文件'.format(df.shape[0],df.shape[1],pages))
+    if xlsxfile is None:
+        xlsxfile = jsonfile.strip('.json') + '.xlsx'
     writer = pd.ExcelWriter(xlsxfile,engine='xlsxwriter', options={'strings_to_urls': False})
     for page in range(1,pages+1):
         start,end = page * max_rows - max_rows + 1,page * max_rows
@@ -30,4 +31,4 @@ if __name__ == '__main__':
     jsonfile = r'D:\PycharmProjects\tongyong_history\model_package\101_20170501_autohome_forum-3.json'
     xlsxfile = r'D:\PycharmProjects\tongyong_history\model_package\101_20170501_autohome_forum-3.xlsx'
 
-    json2xlsx(jsonfile,xlsxfile)
+    json2xlsx(jsonfile)
