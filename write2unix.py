14af320690b7f1cb834cf2c88592bc7a8a2d5b1a
wangyanglongcc <wangyanglongcc@users.noreply.github.com>
2019-07-05 15:01:22 +0800

    writer2unix
    write df with unix style

---
+++ write2unix.py
@@ -0,0 +1,28 @@
+def _write2unix(df,filepath):
+    # title
+    _title = [str(x) for x in df.columns]
+    _title = '\t'.join(_title) + '\n'
+    # content
+    _content = []
+    for _row in df.values:
+        _row = [str(x) for x in _row]
+        _row = '\t'.join(_row)
+        _content.append(_row)
+    _content = '\n'.join(_content)+'\n'
+    # write2output
+    data = _title + _content
+    data = data.encode()
+    EOL_UNIX = b'\n'
+    EOL_WINDOWS = b'\r\n'
+    EOL_MAC = b'\r'
+    data = data.replace(EOL_WINDOWS, EOL_UNIX).replace(EOL_MAC, EOL_UNIX)
+    f = open(filepath, 'wb')
+    f.write(data)
+    f.close()
+
+if __name__ == '__main__':
+    import pandas as pd
+    import random as rd
+    df = pd.DataFrame({'index':list(range(10)),
+                       'value':[rd.choice(['a','b']) for i in range(10)]})
+    _write2unix(df,'df2unix.txt')
