a5407d5d1acc9dcb902d93f26fdbc588beba4115
wangyanglongcc <41174364+wangyanglongcc@users.noreply.github.com>
2019-07-12 17:33:10 +0800

    dt.timedelta

--- dayofyear.py
+++ dayofyear.py
@@ -1,6 +1,7 @@
 import datetime as dt
 
 def dayofyear(yyyymmdd):
+    '''如果需要指定从周一或周日或其他周次开始计算，可使用dt.timedelta通过日期的增减来变换'''
     return dt.datetime.strptime(str(yyyymmdd), '%Y%m%d').isocalendar()[1]
 
 
